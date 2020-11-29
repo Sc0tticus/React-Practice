e0519a74d97817c39ff2a35eac90ab52b471578d
Sc0tticus <ssinger303@gmail.com>
2020-11-28 21:17:33 -0700

    Created App component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
+// const App = function () {
+// 	return <div>Hi there!</div>;
+// };
+// ES2015 syntax
+const App = () => {
+	return <div>Hi there!</div>;
+};
 
 // Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
